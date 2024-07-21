63fb508a938ce967b7989b15ad72094a8791a9d2
SultanHello <sultanasimbekov@mail.ru>
2024-07-21 21:20:52 +0500

    first commit

---
+++ src/test/java/com/example/AvitoDemo/AvitoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AvitoDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvitoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
